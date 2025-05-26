22b4f2a2dc213c6a3d2238ada4b123d3c1548fee
nozomihuynh <ares_blaze@yahoo.com.vn>
2025-05-26 21:09:38 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Discord_Bot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+//using System;
+using DSharpPlus.EventArgs;
+using System.Threading.Tasks;
+
+namespace Discord_Bot
+{
+    internal class Program
+    {
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextExtension Commands { get; set; } 
+        static async Task Main(string[] args)
+        {
+            var jsonReader = new JSONReader();
+            await jsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = jsonReader.token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true
+            };
+
+            Client = new DiscordClient(discordConfig);
+            Client.Ready += Client_Ready;
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);   //Assure bot stay online as long as program is on
+        }
+
+        private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
